d64fb868ee500a578cbac572a8742e3ca5338ba8
yasmin <ydeoliveira016@gmail.com>
2025-02-18 11:48:27 -0300

    Yasmin

--- aula-01/app.py
+++ aula-01/app.py
@@ -1,15 +1,24 @@
 #Importando o Flask 
-from flask import Flask
+from flask import Flask, render_template
 
 #Carregando o Flask na varável app
-app = Flask(__name__)
+app = Flask(__name__, template_folder='views')
 
 #Criando aprimeira rota do site
 @app.route('/')
 
 #Criando função no python
 def home():
-    return'<h1>bem-vindo ao meu primeiro site em Flask!</h1>'
+    return render_template('index.html')
+
+@app.route('/games')
+def games():
+    título = 'CS-GO'
+    ano = 2012
+    categoria = 'FPS Online'
+    jogadores = ['Julia' , 'eu']
+    jogos = ['Minecraft', 'Freefire', 'Amongus', 'GTA', 'Pokemon', 'Roblox' ] 
+    return render_template('games.html', título=título, ano=ano, categoria=categoria, jogadores=jogadores, jogos=jogos)
 
 
 #iniciando o sevidor na localhost, porta 5000, modo de dpuração ativado
